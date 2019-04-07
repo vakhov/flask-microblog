5a9e682b0c71165ef274912ed256296ffed7b9ef
Alexander Vakhov <alexander@vakhov.ru>
2019-04-07 11:43:32 +0500

    validate form login

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
+from flask import flash
+from flask import redirect
 from flask import render_template
 from app import app
+from .forms import LoginForm
 
 
 @app.route('/')
@@ -17,3 +20,12 @@ def index():
         },
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login required for OpenID={0}, remember_me={1}'.format(form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
