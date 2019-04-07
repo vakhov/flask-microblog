a7d570794c0032360c66c3515dd7cd26ca4d5827
Alexander Vakhov <alexander@vakhov.ru>
2019-04-07 10:43:42 +0500

    cycle for in template

--- app/views.py
+++ app/views.py
@@ -6,4 +6,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Alexander'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'boyd': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'boyd': 'The Avengers movie was so cool!'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
