e23de6444382a21c12d58ca68e9e9729107f862d
Alexander Vakhov <alexander@vakhov.ru>
2019-04-07 16:26:29 +0500

    add models user and post

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ OPENID_PROVIDERS = [
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
