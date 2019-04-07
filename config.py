d7fa21035b993253cd9fdd19dbf2f31dabad41f1
Alexander Vakhov <alexander@vakhov.ru>
2019-04-07 11:55:10 +0500

    Взаимодействие с OpenID

--- config.py
+++ config.py
@@ -2,3 +2,10 @@ import os
 
 CSRF_ENABLED = True
 SECRET_KEY = os.environ['SECRET_KEY']
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
